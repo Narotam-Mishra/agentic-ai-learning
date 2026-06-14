4595bb250f5b3f7f9af2c7c0fc9636c5243d86d0
Narotam-Mishra <narotam21jan@gmail.com>
2026-06-14 21:22:17 +0530

    agentic-ai_chatbot_buiding_1.1

---
+++ agentic_ai_campusX/08_chatbot_using_langgraph/01_chatbot_workflow.py
@@ -0,0 +1,82 @@
+
+# chatbot workflow
+
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
+from typing import TypedDict, Literal, Annotated
+from pydantic import BaseModel, Field
+from IPython.display import Image, display
+from pathlib import Path
+import operator
+
+load_dotenv(override=True)
+
+# define state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# define openAI LLM model
+llm = ChatOpenAI()
+
+def chat_node(state: ChatState):
+    # take user query from state
+    messages = state['messages']
+
+    # send query to llm
+    response = llm.invoke(messages)
+
+    # store response to state
+    return{
+        'messages': [response]
+    }
+
+
+# create graph workflow
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+# add edges
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+# compile graph
+chatbot_workflow = graph.compile()
+
+# initial_state = {
+#     'messages': [
+#         HumanMessage(content='What is the Capital of South Africa')
+#     ]
+# }
+
+while True:
+    user_message = input('type here... ')
+    print(f"User's Message: {user_message}")
+
+    if user_message.strip().lower() in ['exit', 'quit', 'bye']:
+        break
+
+    final_res = chatbot_workflow.invoke({
+        'messages': [HumanMessage(content=user_message)]
+    })
+
+    # print(f"AI Response: {final_res}")
+    print(f"query_response: {final_res['messages'][-1].content}")
+
+# get your graph image
+png_data = chatbot_workflow.get_graph().draw_mermaid_png()
+display(Image(png_data))
+
+images_dir = Path(__file__).parent / "images"
+images_dir.mkdir(exist_ok=True)
+
+with open(images_dir / "chatbot_workflow.png", "wb") as f:
+    f.write(png_data)
+
+print(chatbot_workflow.get_graph().draw_mermaid())
+
+
